0ebc36b993bfc7aa239bd4d355b98062daaaca71
Александр Ефанов <verpl01@yandex.ru>
2020-07-13 11:53:01 +0500

    Bulma & Vue Components

---
+++ Shop.UI/wwwroot/js/admin/product-component.js
@@ -0,0 +1,153 @@
+﻿Vue.component('product-manager', {
+    template: `<div v-if="!editing">
+                    <button class="button" @@click="newProduct">Add new product</button>
+                    <table class="table">
+                        <tr>
+                            <th>Id</th>
+                            <th>Product</th>
+                            <th>Value</th>
+                        </tr>
+                        <tr v-for="product in products">
+                            <td>{{product.id}}</td>
+                            <td>{{product.name}}</td>
+                            <td>{{product.value}}</td>
+                            <td><a @@click="editProduct(product.id, index)">Edit</a></td>
+                            <td><a @@click="deleteProduct(product.id, index)">Remove</a></td>
+                        </tr>
+                    </table>
+                </div>
+                <div v-else>
+                    <div class="field">
+                        <label class="label">Product Name</label>
+                        <div class="control">
+                            <input class="input" v-model="productModel.name" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">Product Description</label>
+                        <div class="control">
+                            <input class="input" v-model="productModel.description" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">Value</label>
+                        <div class="control">
+                            <input class="input" v-model="productModel.value" />
+                        </div>
+                    </div>
+                    <button class="button is-success" @@click="createProduct" v-if="!productModel.id">Create product</button>
+                    <button class="button is-warning" @@click="updateProduct" v-else>Update product</button>
+                    <button class="button" @@click="cancel">Cancel</button>
+                </div>`,
+    data() {
+        return {
+            editing: false,
+            loading: false,
+            objectIndex: 0,
+            productModel: {
+                id: 0,
+                name: "Product Name",
+                description: "Product Description",
+                value: 1.99
+        },
+            products: []
+        }
+    },
+    mounted() {
+        this.getProducts();
+    },
+    methods: {
+        getProducts() {
+            this.loading = true;
+            axios.get('/Admin/products')
+                .then(res => {
+                    console.log(res);
+                    this.products = res.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        getProduct(id) {
+            this.loading = true;
+            axios.get('/Admin/products/' + id)
+                .then(res => {
+                    console.log(res);
+                    var product = res.data;
+                    this.productModel = {
+                        id: product.id,
+                        name: product.name,
+                        description: product.description,
+                        value: product.value
+                    };
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        createProduct() {
+            this.loading = true;
+            axios.post('/Admin/products', this.productModel)
+                .then(res => {
+                    console.log(res.data);
+                    this.products.push(res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                    this.editing = false;
+                });
+        },
+        editProduct(id, index) {
+            this.objectIndex = index;
+            this.getProduct(id);
+            this.editing = true;
+        },
+        updateProduct() {
+            this.loading = true;
+            axios.put('/Admin/products', this.productModel)
+                .then(res => {
+                    console.log(res.data);
+                    this.products.splice(this.objectIndex, 1, res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                    this.editing = false;
+                });
+        },
+        deleteProduct(id, index) {
+            this.loading = true;
+            axios.delete('/Admin/products/' + id)
+                .then(res => {
+                    console.log(res);
+                    this.products.splice(index, 1);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        newProduct() {
+            this.editing = true;
+            this.productModel.id = 0;
+        },
+        cancel() {
+            this.editing = false;
+        }
+    },
+    computed: {
+    }
+});

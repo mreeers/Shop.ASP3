fd80939169072b08bfc0541a053a127495c09883
Александр Ефанов <verpl01@yandex.ru>
2020-07-24 15:22:20 +0500

    Stock Management

---
+++ Shop.Application/StockAdmin/GetStocks.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.Application.StockAdmin
+{
+    public class GetStocks
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetStocks(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<ProductViewModel> Do()
+        {
+            var stock = _context.Products
+                .Include(x => x.Stocks)
+                .Select(x => new ProductViewModel
+                { 
+                    Id = x.Id,
+                    Description = x.Description,
+                    Stock = x.Stocks.Select(y => new StockViewModel
+                    {
+                        Id = y.Id,
+                        Description = y.Description,
+                        Qty = y.Qty,
+                    })
+                })
+                .ToList();
+
+            return stock;
+        }
+
+        public class StockViewModel
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public int Qty { get; set; }
+        }
+
+        public class ProductViewModel
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public IEnumerable<StockViewModel> Stock { get; set; }
+        }
+    }
+}
--- Shop.UI/Controllers/AdminController.cs
+++ Shop.UI/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Application.ProductsAdmin;
+using Shop.Application.StockAdmin;
 using Shop.Database;
 
 namespace Shop.UI.Controllers
@@ -31,6 +32,19 @@ namespace Shop.UI.Controllers
         public async Task<IActionResult> DeleteProduct(int id) => Ok(await new DeleteProduct(_context).Do(id));
 
         [HttpPut("products")]
-        public async Task<IActionResult> UpdateProduct([FromBody] UpdateProduct.Request vm) => Ok(await new UpdateProduct(_context).Do(vm));
+        public async Task<IActionResult> UpdateProduct([FromBody] UpdateProduct.Request request) => Ok(await new UpdateProduct(_context).Do(request));
+
+
+        [HttpGet("stocks")]
+        public IActionResult GetStock() => Ok(new GetStocks(_context).Do());
+
+        [HttpPost("stocks")]
+        public async Task<IActionResult> CreateStock([FromBody] CreateStock.Request request) => Ok(await new CreateStock(_context).Do(request));
+
+        [HttpDelete("stocks/{id}")]
+        public async Task<IActionResult> DeleteStock(int id) => Ok(await new DeleteStock(_context).Do(id));
+
+        [HttpPut("stocks")]
+        public async Task<IActionResult> UpdateStock([FromBody] UpdateStock.Request request) => Ok(await new UpdateStock(_context).Do(request));
     }
 }

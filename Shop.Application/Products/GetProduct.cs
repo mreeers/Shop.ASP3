8994670f36f6f32ff724e3bc3b9f8e1d1e43822d
Александр Ефанов <verpl01@yandex.ru>
2020-07-24 17:15:42 +0500

    Displaying Products

---
+++ Shop.Application/Products/GetProduct.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.Application.Products
+{
+    public class GetProduct
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetProduct(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public ProductViewModel Do(string name) =>
+        _context.Products
+            .Include(x => x.Stocks)
+            .Where(x => x.Name == name)
+            .Select(x => new ProductViewModel
+            {
+                Name = x.Name,
+                Description = x.Description,
+                Value = $"$ {x.Value.ToString("N2")}", //1100.50 => 1,100.50
+
+                Stock = x.Stocks.Select(y => new StockViewModel
+                {
+                    Id = y.Id,
+                    Description = y.Description,
+                    InStock = y.Qty > 0
+                })
+            })
+            .FirstOrDefault();
+
+        public class ProductViewModel
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Value { get; set; }
+            public IEnumerable<StockViewModel> Stock { get; set; }
+        }
+
+        public class StockViewModel
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public bool InStock { get; set; }
+        }
+    }
+}

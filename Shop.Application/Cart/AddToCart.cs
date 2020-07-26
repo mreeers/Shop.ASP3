b74fdc7722f9e78ed4ca29a325aceb29544566a6
Александр Ефанов <verpl01@yandex.ru>
2020-07-26 22:28:35 +0500

    Shopping Cart Part 1

---
+++ Shop.Application/Cart/AddToCart.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Shop.Domain.Models;
+
+namespace Shop.Application.Cart
+{
+    public class AddToCart
+    {
+        private readonly ISession _session;
+
+        public AddToCart(ISession session)
+        {
+            _session = session;
+        }
+
+        public class Request
+        {
+            public int StockId { get; set; }
+            public int Qty { get; set; }
+        }
+
+        public void Do(Request request)
+        {
+            var cartProduct = new CartProduct
+            {
+                StockId = request.StockId,
+                Qty = request.Qty
+            };
+
+            var stringObject = JsonConvert.SerializeObject(request);
+
+            //TODO: appending the cart
+
+            _session.SetString("cart", stringObject);
+        }
+    }
+}
---
+++ Shop.Application/Cart/GetCart.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Shop.Database;
+using Shop.Domain.Models;
+using System.Linq;
+
+namespace Shop.Application.Cart
+{
+    public class GetCart
+    {
+        private readonly ISession _session;
+        private readonly ApplicationDbContext _context;
+
+        public GetCart(ISession session, ApplicationDbContext context)
+        {
+            _session = session;
+            _context = context;
+        }
+
+        public class Response
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+            public int StockId { get; set; }
+            public int Qty { get; set; }
+        }
+
+        public Response Do()
+        {
+            var stringObject = _session.GetString("cart");
+
+            var cartProduct = JsonConvert.DeserializeObject<CartProduct>(stringObject);
+
+            var response = _context.Stocks
+                .Include(x => x.Product)
+                .Where(x => x.Id == cartProduct.StockId)
+                .Select(x => new Response
+                {
+                    Name = x.Product.Name,
+                    Value = $" {x.Product.Value.ToString("N2")}",
+                    StockId = x.Id,
+                    Qty = cartProduct.Qty
+                })
+                .FirstOrDefault();
+
+            return new Response();
+        }
+    }
+}
